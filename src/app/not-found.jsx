e3ee2838dab42f75f96c2e2c29f581efbe3ecc4b
Souvik Sarkar <12345souvik12345@gmail.com>
2024-02-03 23:24:27 +0530

    Not Found Added

---
+++ src/app/not-found.jsx
@@ -0,0 +1,61 @@
+"use client";
+import React, { useEffect } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import "../css/notfound.css";
+import Link from "next/link";
+
+const NotFoundPage = () => {
+  useGSAP(() => {
+    gsap.from(".navbar > div", {
+      opacity: 0,
+      y: 60,
+      ease: "expo.inOut",
+      delay: 2.4,
+    });
+
+    gsap.from(".site-logo", {
+      opacity: 0,
+      y: 40,
+      ease: "expo.inOut",
+      delay: 2.4,
+    });
+
+    gsap.from(".header > span", {
+      top: "-100vh",
+      ease: "bounce.out",
+      delay: 1,
+      stagger: 0.2,
+    });
+
+    gsap.to(".footer span", {
+      y: -40,
+      ease: "expo.inOut",
+      delay: 2.4,
+    });
+  }, []);
+
+  return (
+    <div className="container absolute">
+      <div className="header-container">
+        <div className="header font-canopee">
+          <span>4</span>
+          <span>0</span>
+          <span>4</span>
+        </div>
+      </div>
+
+      <div className="footer font-mono text-xl">
+        <span>
+          Seems you are lost, come back{" "}
+          <Link className=" z-10 cursor-pointer" href={"/"}>
+            Home
+          </Link>
+        </span>
+        <div className="footer-wrapper"></div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
