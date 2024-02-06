9ec4df39d1184e46f9464a88c9fc87a5c3627414
Souvik Sarkar <12345souvik12345@gmail.com>
2024-02-06 18:03:23 +0530

    Contact Added

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,13 @@
 import Hero from "@/components/Hero/Hero";
 import Preloader from "@/components/Preloader/Preloader";
 import { useRef } from "react";
-import Header from "../components/header";
-import StickyCursor from "../components/stickyCursor";
 
 const Page = () => {
   const stickyElement = useRef(null);
   return (
     <>
       <Preloader />
-      {/*  <Header ref={stickyElement} />
-      <StickyCursor stickyElement={stickyElement} /> */}
+
       <Hero />
     </>
   );
